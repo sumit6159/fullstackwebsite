4af63515a817919285658974cb6d99c5e5749016
sumit6159 <90570005+sumit6159@users.noreply.github.com>
2022-06-03 02:58:18 +0530

    somechnages

--- backend/app.js
+++ backend/app.js
@@ -23,8 +23,8 @@ const user = require("./routes/userRoute");
 // const order = require("./routes/orderRoute");
 // const payment = require("./routes/paymentRoute");
 
-app.use("/", product);
-app.use("/", user);
+app.use("/api/v1", product);
+app.use("/api/v1", user);
 // app.use("/api/v1", order);
 // app.use("/api/v1", payment);
 
